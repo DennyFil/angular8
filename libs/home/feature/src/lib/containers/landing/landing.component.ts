7a87c81813251223c3a003c2ce5846aa7ba9c238
Denis Filimonov <denis.filimonov.mail@gmail.com>
2020-08-24 23:01:17 +0200

    added landing page component

---
+++ libs/home/feature/src/lib/containers/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'angular8-course-ws-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
