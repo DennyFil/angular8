3377f626c440fb739e6a01d5f48b8c3002a805f3
Denis Filimonov <denis.filimonov.mail@gmail.com>
2020-08-25 22:21:37 +0200

    added email control basic layout

--- libs/home/feature/src/lib/containers/landing/landing.component.ts
+++ libs/home/feature/src/lib/containers/landing/landing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'angular8-course-ws-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
 
