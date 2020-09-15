9961980895bc7c94f01715aed16b35d206cfa2f3
Denis Filimonov <denis.filimonov.mail@gmail.com>
2020-09-16 00:22:21 +0200

    styled our email control

--- libs/email-input/src/lib/email-input.module.ts
+++ libs/email-input/src/lib/email-input.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from '@angular8-course-ws/angular-material';
 import { EmailInputComponent } from './email-input/email-input.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, AngularMaterialModule],
   exports: [EmailInputComponent],
   declarations: [EmailInputComponent],
 })
